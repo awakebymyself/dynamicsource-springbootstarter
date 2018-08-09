93801b31e82aa609837cdda0001e6dfcda29b9d3
刘志钢 <liuzhigang@ymatou.com>
2018-08-09 18:23:34 +0800

    增加datasource自动配置bean

--- src/main/java/com/lzg/dynamicsource/DynamicDSAutoConfiguration.java
+++ src/main/java/com/lzg/dynamicsource/DynamicDSAutoConfiguration.java
@@ -1,15 +1,19 @@
 package com.lzg.dynamicsource;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import com.lzg.dynamicsource.config.DaoMapperAdvice;
 import com.lzg.dynamicsource.regist.DbObject;
 import org.springframework.aop.Advisor;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 /**
  * Short for dynamic-data-source
@@ -21,6 +25,21 @@ public class DynamicDSAutoConfiguration {
 
     @Autowired
     private DynamicDsProperties dsProperties;
+
+    @Bean
+    @ConditionalOnMissingBean(DataSource.class)
+    public DataSource dataSource() {
+        DynamicDataSource dynamicDataSource = new DynamicDataSource();
+        dynamicDataSource.setDefaultTargetDataSource(defaultDataSource());
+
+        return dynamicDataSource;
+    }
+
+    private DataSource defaultDataSource() {
+        DruidDataSource druidDataSource = new DruidDataSource();
+
+        return druidDataSource;
+    }
 
     @Bean
     public AspectJExpressionPointcut pointcut() {
